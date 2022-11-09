c4bbdf167fc2ac15275ea7320a3c5fe08f37e1fb
claranocito <clara_nocito@hotmail.com>
2022-11-09 14:18:24 -0300

    adjust contact component

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -8,11 +8,12 @@ const Contact = () => {
     return (
         <div className='contact-container'>
            <div className='contacts'>
-            <div className='oi'><a href='https://atendimento.descomplica.com.br/hc/pt-br' target='_blank'>Central de Ajuda</a></div>
-            <div className='oi'><a href='https://descomplica.com.br/sobre/quem-somos/' target='_blank'>Quem Somos</a></div>
-            <div className='oi'><a href='https://descomplica.com.br/sobre/politica-de-privacidade/' target='_blank'>Politica de Privacidade</a></div>
-            <div className='oi'><a href='https://descomplica.com.br/sobre/termos-de-uso/' target='_blank'>Termos de Uso</a></div>
-            <div className='oi'><a href='https://boards.greenhouse.io/descomplica' target='_blank'>Trabalhe Conosco</a></div>
+                <div className='link'><a href='https://atendimento.descomplica.com.br/hc/pt-br' target='_blank'>Central de Ajuda</a></div>
+                <div className='link'><a href='https://descomplica.com.br/sobre/quem-somos/' target='_blank'>Quem Somos</a></div>
+                <div className='link'><a href='https://descomplica.com.br/sobre/politica-de-privacidade/' target='_blank'>Politica de Privacidade</a></div>
+
+                <div className='link'><a href='https://descomplica.com.br/sobre/termos-de-uso/' target='_blank'>Termos de Uso</a></div>
+                <div className='link'><a href='https://boards.greenhouse.io/descomplica' target='_blank'>Trabalhe Conosco</a></div>
            </div>
 
            <div className='redes'>
