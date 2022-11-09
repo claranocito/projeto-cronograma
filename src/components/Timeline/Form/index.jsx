61c463c7302278ddc2454c84754cbdf87ebe565f
Bruno Cesar Nogueira dos Santos <33167552+brunocnsantos@users.noreply.github.com>
2022-11-09 16:30:55 -0300

    new pastas

---
+++ src/components/Timeline/Form/index.jsx
@@ -0,0 +1,12 @@
+import './styles.css'
+
+const Timeline = () => {
+    return (
+        <div className='container-timeline'>
+            <h2 className='h2-timeline'>MONTE AQUI SEU <strong>CRONOGRAMA DE ESTUDOS PERFEITO</strong></h2>
+            <div className='timeline'>cronograma</div>
+        </div>
+    )
+}
+
+export {Timeline}
